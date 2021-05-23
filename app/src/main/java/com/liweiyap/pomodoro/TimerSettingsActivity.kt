df6e48e9c5326cbab4cf98184aa6e5f9460ac989
liweiyap <liweiyap@gmail.com>
2021-05-23 14:17:48 +0100

    Adjust settings of basic spinner

--- app/src/main/java/com/liweiyap/pomodoro/TimerSettingsActivity.kt
+++ app/src/main/java/com/liweiyap/pomodoro/TimerSettingsActivity.kt
@@ -3,11 +3,12 @@ package com.liweiyap.pomodoro
 import android.os.Bundle
 import android.view.View
 import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import android.widget.Spinner
 import androidx.appcompat.app.AppCompatActivity
 import com.liweiyap.pomodoro.utils.SpinnerManager
 
-class MainActivity : AppCompatActivity()
+class TimerSettingsActivity : AppCompatActivity()
 {
     override fun onCreate(savedInstanceState: Bundle?)
     {
@@ -15,6 +16,7 @@ class MainActivity : AppCompatActivity()
         setContentView(R.layout.activity_main)
 
         val settingsSetNumberSpinner: Spinner = findViewById(R.id.settingsSetNumberSpinner)
+        settingsSetNumberSpinner.adapter = ArrayAdapter(this, R.layout.spinner_item_textview, resources.getStringArray(R.array.setNumberArray))
         settingsSetNumberSpinner.onItemSelectedListener = object: AdapterView.OnItemSelectedListener
         {
             override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long)
